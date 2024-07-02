3d3dccd77898e5b0cc82346236de1202f3804c45
Jens P <jens.poldme@heathmont.net>
2024-07-02 14:02:49 +0300

    downgrade supported iOS version to 16

--- Package.swift
+++ Package.swift
@@ -1,13 +1,15 @@
 // swift-tools-version: 5.10
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
     name: "LiveViewNativeFacebookSignIn",
-    platforms: [.iOS(.v17), .macOS(.v14), .tvOS(.v17), .watchOS(.v10)],
+    platforms: [
+        .iOS(.v16),
+        .macOS(.v13),
+        .tvOS(.v16),
+        .watchOS(.v9)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "LiveViewNativeFacebookSignIn",
             targets: ["LiveViewNativeFacebookSignIn"]),
@@ -17,8 +19,6 @@ let package = Package(
         .package(url: "https://github.com/facebook/facebook-ios-sdk", .upToNextMajor(from: "17.0.2")),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "LiveViewNativeFacebookSignIn",
             dependencies: [
